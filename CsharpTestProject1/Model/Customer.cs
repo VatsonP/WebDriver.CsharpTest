71a9b2c5da68ea81c4661254c45451c646512703
VatsonP <58036174+VatsonP@users.noreply.github.com>
2020-11-14 17:08:55 +0200

    Add base files
    Add base files

---
+++ CsharpTestProject1/Model/Customer.cs
@@ -0,0 +1,18 @@
+﻿namespace CsharpTestProject1
+{
+    public class Customer
+    {
+        public string Tax_id { get; internal set; }
+        public string Company { get; internal set; }
+        public string Address1 { get; internal set; }
+        public string City { get; internal set; }
+        public string Country { get; internal set; }
+        public string Email { get; internal set; }
+        public string Firstname { get; internal set; }
+        public string Lastname { get; internal set; }
+        public string Phone { get; internal set; }
+        public string Postcode { get; internal set; }
+        public string Zone { get; internal set; }
+        public string Password { get; internal set; }
+    }
+}
---
+++ CsharpTestProject1/TestBase/TestBase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+
+
+namespace CsharpTestProject1
+{
+
+    [TestFixture]
+    public abstract class TestBase
+    {
+        public DriverBase DrvBase
+        { 
+            get;
+            private set;
+        }
+
+        public TestBase(DriverBaseParams driverBaseParams) 
+        {
+            DrvBase = new DriverBase(driverBaseParams);
+        }
+
+        [OneTimeSetUp]
+        protected static void OneTimeSetUp()
+        {
+            DriverBase.OneTimeSetUp();
+        }
+
+        [SetUp]
+        protected void SetUp()
+        {
+            DrvBase.SetUp();
+        }
+
+        [TearDown]
+        protected void TearDown()
+        {
+            DrvBase.TearDown();
+        }
+
+        [OneTimeTearDown]
+        protected static void OneTimeTearDown()
+        {
+            DriverBase.OneTimeTearDown();
+        }
+    }
+}
